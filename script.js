e1750d985b4ffa0ddd8b34bb2f2a777666895a7c
Philipp Duck <phil.mar.du@gmail.com>
2022-02-04 21:04:44 +0100

    added viewToggle Button / UI

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const anlagenKennzahlen = [
         inventarnummer: 9306799,
         kostenstelle: 3271,
         kennzahlen: {
-            taktzeit: true,
+            taktzeit: false,
             mehrfachlaeuferquote: true,
             bildverarbeitung: true,
             schrauber: false,
@@ -316,13 +316,14 @@ const anlagenFlops = [
 
 ]
 
-function buildGrid() {
+function buildGrid(anlagen, showAll) {
 
-    const anlagenLenght = Object.keys(anlagenKennzahlen).length;
+    const anlagenLenght = Object.keys(anlagen).length;
 
     console.log(anlagenLenght);
 
-    for (const [anlagenKey, anlagenValue] of Object.entries(anlagenKennzahlen)) {
+    for (const [anlagenKey, anlagenValue] of Object.entries(anlagen)) {
+        let anlageNio = false;
         const main = document.getElementById("main");
 
         const anlage = document.createElement("div");
@@ -341,6 +342,7 @@ function buildGrid() {
         kennzahlen.className = "kennzahlen";
     
         for (const [key, value] of Object.entries(anlagenValue["anlage"]["kennzahlen"]))  {
+            let kennzahlNio = false;
             //console.log(`${key}: ${value}`);
             const kennzahl = document.createElement("div");
             kennzahl.className = "kennzahl";
@@ -348,11 +350,11 @@ function buildGrid() {
             if (key === "taktzeit") {
                 kennzahl.innerHTML = "<img src='img/clock.png'> Taktzeit";
             } else if (key === "mehrfachlaeuferquote") {
-                kennzahl.innerHTML = "<img src='img/dlq.png'> DLQ";
+                kennzahl.innerHTML = "<img src='img/dlq.png'> Mehrfach-</br>läuferquote";
             } else if (key === "bildverarbeitung") {
-                kennzahl.innerHTML = "<img src='img/cam.png'> IBV";
+                kennzahl.innerHTML = "<img src='img/cam.png'> Kamera-</br>systeme";
             } else if (key === "schrauber") {
-                kennzahl.innerHTML = "<img src='img/drill.png'> Schrauber";
+                kennzahl.innerHTML = "<img src='img/drill.png'> Schraub-</br>technik";
             }
 
             const classname = kennzahl.className;
@@ -360,15 +362,40 @@ function buildGrid() {
                 kennzahl.className = `${classname} io`;
             } else {
                 kennzahl.className = `${classname} nio`;
+                anlageNio = true;
+                kennzahlNio = true;
             }
-    
-            kennzahlen.append(kennzahl);
+            if (showAll === true || kennzahlNio) {
+                kennzahlen.append(kennzahl);
+                kennzahlNio = false;
+            }
         }
-    
+        
         anlage.append(kennzahlen);
-    
-        main.append(anlage);
+        
+        if (anlageNio || showAll) {
+            main.append(anlage);
+        }
     }
 }
 
-window.onload = buildGrid();
+
+window.onload = buildGrid(anlagenKennzahlen, false);
+
+let viewAll = false;
+function toggleViewAll() {
+    const main = document.getElementById("main");
+    while (main.firstChild) {
+        main.removeChild(main.lastChild);
+    }
+    const button = document.getElementById("viewToggleButton");
+    if (viewAll) {
+        buildGrid(anlagenKennzahlen, false);
+        button.innerText = "Alle anzeigen";
+        viewAll = false;
+    } else {
+        buildGrid(anlagenKennzahlen, true);
+        button.innerText = "Zeige nur NIO´s";
+        viewAll = true;
+    }
+}
