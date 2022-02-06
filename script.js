79dfe20adf1f13380e9b55543a2e203e3dbc9c92
Philipp Duck <phil.mar.du@gmail.com>
2022-02-06 09:46:50 +0100

    a lot of changes
    - JSON ausgelagert
    - button + Funktion Zufallsdaten
    - sortieralgorithmus (funktioniert nicht richtig)

--- script.js
+++ script.js
@@ -1,329 +1,17 @@
+"use strict"
 
-
-const anlagenKennzahlen = [
-    {anlage: {
-        name: "RCEPS Lenkung 1",
-        inventarnummer: 9305306,
-        kostenstelle: 3172,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "RCEPS Sensor 2",
-        inventarnummer: 9306582,
-        kostenstelle: 3172,
-        kennzahlen: {
-            taktzeit: false,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MLB-EVO Lenkung 1",
-        inventarnummer: 9306997,
-        kostenstelle: 3271,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MLB-EVO Lenkung 3",
-        inventarnummer: 9306799,
-        kostenstelle: 3271,
-        kennzahlen: {
-            taktzeit: false,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: false,
-        }
-    }},
-    {anlage: {
-        name: "MLB-EVO Lenkung 4",
-        inventarnummer: 9308631,
-        kostenstelle: 3271,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MLB-EVO Sensor 1",
-        inventarnummer: 9305284,
-        kostenstelle: 3271,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MQB Lenkung 1",
-        inventarnummer: 9306581,
-        kostenstelle: 3278,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MQB Lenkung 2",
-        inventarnummer: 9307526,
-        kostenstelle: 3278,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: false,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MQB Lenkung 3",
-        inventarnummer: 9308129,
-        kostenstelle: 3278,
-        kennzahlen: {
-            taktzeit: false,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MQB Sensor 1",
-        inventarnummer: 9306599,
-        kostenstelle: 3278,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MQB Sensor 2",
-        inventarnummer: 9308250,
-        kostenstelle: 3278,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "PPE Lenkung",
-        inventarnummer: 9310017,
-        kostenstelle: 3280,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "PPE Sensor",
-        inventarnummer: "nicht definiert",
-        kostenstelle: 3280,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: false,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MEB Lenkung",
-        inventarnummer: 9308672,
-        kostenstelle: 3279,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-
-]
-
-const anlagenFlops = [
-    {anlage: {
-        name: "RCEPS Lenkung 1",
-        inventarnummer: 9305306,
-        kostenstelle: 3172,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "RCEPS Sensor 2",
-        inventarnummer: 9306582,
-        kostenstelle: 3172,
-        kennzahlen: {
-            taktzeit: false,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MLB-EVO Lenkung 1",
-        inventarnummer: 9306997,
-        kostenstelle: 3271,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MLB-EVO Lenkung 3",
-        inventarnummer: 9306799,
-        kostenstelle: 3271,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: false,
-        }
-    }},
-    {anlage: {
-        name: "MLB-EVO Lenkung 4",
-        inventarnummer: 9308631,
-        kostenstelle: 3271,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MLB-EVO Sensor 1",
-        inventarnummer: 9305284,
-        kostenstelle: 3271,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MQB Lenkung 1",
-        inventarnummer: 9306581,
-        kostenstelle: 3278,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MQB Lenkung 2",
-        inventarnummer: 9307526,
-        kostenstelle: 3278,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: false,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MQB Lenkung 3",
-        inventarnummer: 9308129,
-        kostenstelle: 3278,
-        kennzahlen: {
-            taktzeit: false,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MQB Sensor 1",
-        inventarnummer: 9306599,
-        kostenstelle: 3278,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MQB Sensor 2",
-        inventarnummer: 9308250,
-        kostenstelle: 3278,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "PPE Lenkung",
-        inventarnummer: 9310017,
-        kostenstelle: 3280,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "PPE Sensor",
-        inventarnummer: 0000000,
-        kostenstelle: 3280,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: false,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-    {anlage: {
-        name: "MEB Lenkung",
-        inventarnummer: 9308672,
-        kostenstelle: 3279,
-        kennzahlen: {
-            taktzeit: true,
-            mehrfachlaeuferquote: true,
-            bildverarbeitung: true,
-            schrauber: true,
-        }
-    }},
-
-]
+let machineOverviewJSOnData;
 
 function buildGrid(anlagen, showAll) {
+    deleteContent();
 
     const anlagenLenght = Object.keys(anlagen).length;
 
-    console.log(anlagenLenght);
-
+    let highestNIOAnlage = {anlage: null, nios: 0};
+    let lowestNIOAnlage = {anlage: null, nios: 0};
     for (const [anlagenKey, anlagenValue] of Object.entries(anlagen)) {
         let anlageNio = false;
+        let anzahlNio = 0;
         const main = document.getElementById("main");
 
         const anlage = document.createElement("div");
@@ -340,7 +28,8 @@ function buildGrid(anlagen, showAll) {
     
         const kennzahlen = document.createElement("div")
         kennzahlen.className = "kennzahlen";
-    
+        
+        // Erstelle das Kennzahlen Grid:
         for (const [key, value] of Object.entries(anlagenValue["anlage"]["kennzahlen"]))  {
             let kennzahlNio = false;
             //console.log(`${key}: ${value}`);
@@ -350,19 +39,22 @@ function buildGrid(anlagen, showAll) {
             if (key === "taktzeit") {
                 kennzahl.innerHTML = "<img src='img/clock.png'> Taktzeit";
             } else if (key === "mehrfachlaeuferquote") {
-                kennzahl.innerHTML = "<img src='img/dlq.png'> Mehrfach-</br>läuferquote";
+                kennzahl.innerHTML = "<img src='img/dlq.png'> Mehrfach<wbr>läuferquote";
             } else if (key === "bildverarbeitung") {
-                kennzahl.innerHTML = "<img src='img/cam.png'> Kamera-</br>systeme";
+                kennzahl.innerHTML = "<img src='img/cam.png'> Kamera<wbr>systeme";
             } else if (key === "schrauber") {
-                kennzahl.innerHTML = "<img src='img/drill.png'> Schraub-</br>technik";
+                kennzahl.innerHTML = "<img src='img/drill.png'> Schraub<wbr>technik";
             }
 
             const classname = kennzahl.className;
-            if (value === true) {
+            if (anlagenValue["anlage"]["status"] !== 1) {
+                kennzahl.className = `${classname} noData`;
+            } else if (value == 1) {
                 kennzahl.className = `${classname} io`;
             } else {
                 kennzahl.className = `${classname} nio`;
                 anlageNio = true;
+                anzahlNio += 1;
                 kennzahlNio = true;
             }
             if (showAll === true || kennzahlNio) {
@@ -370,32 +62,95 @@ function buildGrid(anlagen, showAll) {
                 kennzahlNio = false;
             }
         }
-        
+
         anlage.append(kennzahlen);
+
+        // Wenn die Anlage NIO-Kennzahlen besitzt wird sie nach "vorne" einsortiert
+        // hat sie mehr NIO-Kennzahlen als die vorherige Anlage wird sie vor 
+        // die letzte NIO-Anlage einsortiert:
+        if (anlageNio) {
+            console.log(title);
+            console.log(`${anzahlNio} - ${highestNIOAnlage["nios"]}`);
+            if (!highestNIOAnlage["anlage"]) {
+                console.log("POOOP");
+                main.insertAdjacentElement("afterbegin", anlage);
+                highestNIOAnlage = {anlage: anlage, nios: anzahlNio}
+                lowestNIOAnlage = {anlage: anlage, nios: anzahlNio}
+            } else if (anzahlNio >= highestNIOAnlage["nios"]) {
+                highestNIOAnlage["anlage"].before(anlage);
+                highestNIOAnlage = {anlage: anlage, nios: anzahlNio} 
+            } else if (anzahlNio > lowestNIOAnlage["nios"]) {
+                lowestNIOAnlage["anlage"].before(anlage);
         
-        if (anlageNio || showAll) {
+            } else {
+                lowestNIOAnlage["anlage"].after(anlage);
+                lowestNIOAnlage = {anlage: anlage, nios: anzahlNio};
+            }
+            //highestNIOAnlage = {anlage: anlage, nios: anzahlNio}
+        } else if (showAll){
             main.append(anlage);
         }
     }
 }
 
-
-window.onload = buildGrid(anlagenKennzahlen, false);
-
 let viewAll = false;
+window.onload = getData();
+
+
+// Wechselt zwischen "Zeige Alle" und "Zeige nur NIO".
 function toggleViewAll() {
+    const button = document.getElementById("viewToggleButton");
+    if (viewAll) {
+        buildGrid(machineOverviewJSOnData, false);
+        button.innerText = "Alle anzeigen";
+        viewAll = false;
+    } else {
+        buildGrid(machineOverviewJSOnData, true);
+        button.innerText = "Zeige nur NIO´s";
+        viewAll = true;
+    }
+}
+
+// löscht den Gesamten Content aus dem <main> Tag.
+function deleteContent() {
     const main = document.getElementById("main");
     while (main.firstChild) {
         main.removeChild(main.lastChild);
     }
-    const button = document.getElementById("viewToggleButton");
-    if (viewAll) {
-        buildGrid(anlagenKennzahlen, false);
-        button.innerText = "Alle anzeigen";
-        viewAll = false;
-    } else {
-        buildGrid(anlagenKennzahlen, true);
-        button.innerText = "Zeige nur NIO´s";
-        viewAll = true;
+}
+
+
+// Holt den Datensatz aus der JSON Datei
+// und baut das Grid
+function getData() {
+    fetch("machineOverview.JSON")
+    .then(res => res.json())
+    .then(data => {
+        machineOverviewJSOnData = data;
+        buildGrid(data, false);
+    })
+}
+
+// Gibt zufällig 1 oder 0 zurück. Die wahrscheinlichkeit 
+// eine 1 zu erhalten ist höher
+function getRandomValue() {
+    let randomInt = Math.floor(Math.random() * 2);
+    if (randomInt === 0) {
+        randomInt = Math.floor(Math.random() * 2);
+        if (randomInt === 0) {
+            randomInt = Math.floor(Math.random() * 2);
+        }
     }
+    return randomInt;
+}
+
+// Randomisiert die Kennzahlen mithilfe der getRandomValue() funktion.
+function randomizeData() {
+    for (let [anlagenKey, anlagenValue] of Object.entries(machineOverviewJSOnData)) { 
+        for (let [key, value] of Object.entries(anlagenValue["anlage"]["kennzahlen"]))  {
+            anlagenValue["anlage"]["kennzahlen"][key] = getRandomValue();
+        }
+    }
+    console.log(machineOverviewJSOnData);
+    buildGrid(machineOverviewJSOnData,viewAll);
 }
